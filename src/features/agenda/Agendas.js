1c351b0f355a70a354652fe3f680e15ce49cf7a6
Domchap90 <domchaple@gmail.com>
2021-06-09 12:05:19 +0100

    Pagination working for fixed number of agendas per page.

--- src/features/agenda/Agendas.js
+++ src/features/agenda/Agendas.js
@@ -1,18 +1,71 @@
-import { selectAgendas } from './agendasSlice';
-import { useSelector } from 'react-redux';
+import { selectAgendas, selectcurrentAgendas, changePage } from './agendasSlice';
+import { useSelector, useDispatch } from 'react-redux';
 import { Agenda } from './Agenda';
-
+import ReactPaginate from 'react-paginate';
+import { useState, useEffect } from 'react';
 
 export function Agendas() {
-    const agendas = useSelector(selectAgendas);    
+    console.log('componenet entered!')
+    const agendas = useSelector(selectAgendas);
     
-    const agendasList = Object.values(agendas).map((a) => {
-        console.log(`id  = ${a.id}, agenda = ${a.agenda}`)
-        return <Agenda id={a.id} agenda={a.agenda} completed={a.completed} key={a.id} />;
-    });  
+    const [activePage, setActivePage] = useState(0);
+    console.log('active page = ', activePage)
+
+    const perPage = 3;
+
+    const changeOffset = (currentPage, itemsPerPage) => currentPage * itemsPerPage;
+    let offset = 0;
+    const [pageCount, setPageCount] = useState(0);
+    let currentAgendas = [];
+    // console.log('currentAgendas', currentAgendas)
+    const [displayedAgendas, setDisplayedAgendas] = useState([]);
+
+    const handlePageClick = ({selected: selectedPage}) => {
+
+        setActivePage(selectedPage);
+        console.log('handlePageClick entered with page ', activePage)
+        offset = changeOffset(selectedPage, perPage);
+        updateAgendas(offset);      
+         
+    };
+
+    const updateAgendas = (startIndex) => {
+        console.log(`updateAgendas entered with start index ${startIndex} & active page ${activePage}`)
+        currentAgendas = agendas.slice(startIndex, startIndex + perPage);
+        setDisplayedAgendas(currentAgendas.map((a) => {
+            console.log(`id  = ${a.id}, agenda = ${a.agenda}`)
+            return <Agenda id={a.id} agenda={a.agenda} completed={a.completed} key={a.id} />;
+        }));
+        
+    }
+    useEffect(() => {
+
+        console.log('useEffect entered.')
+        setPageCount(Math.ceil(agendas.length/perPage));
+        offset = changeOffset(activePage, perPage);
+        updateAgendas(offset);   
+
+    }, [agendas]);
 
     return (
-        <div className="agendaContainer" > {agendasList}</div>
+        <div>
+            <div id="agendaContainer" > 
+                {displayedAgendas}
+                
+            </div>
+            <div className="paginationContainer" >
+                <ReactPaginate
+                    pageCount={pageCount}
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={10}
+                    marginPagesDisplayed={0}
+                    breakLabel={"..."}
+                    containerClassName={'pagination'}
+                    previousLabel={'<'}
+                    nextLabel={'>'}
+                />
+            </div>
+        </div>
     );
           
 }
--- src/features/agenda/agendasSlice.js
+++ src/features/agenda/agendasSlice.js
@@ -2,28 +2,50 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const agendasSlice = createSlice({
     name: 'agendas',
-    initialState: {agendas: {}},
+    initialState: {
+        agendas: [],
+        displayedAgendas: [],
+        activePage: 1,
+        perPage: 3,
+    },
     reducers: {
         addAgenda: (state, action) => {
             const { agendaId, agenda } = action.payload;
-            state.agendas[agendaId] = {
+            state.agendas.push({
                 id: agendaId,
                 agenda: agenda,
                 completed: false
-            };
+            });
             return state;
         },
         removeAgenda: (state, action) => {
-            delete state.agendas[action.payload];
+            for (let a of state.agendas) {
+                if (a.id === action.payload) {
+                    let ind =state.agendas.indexOf(a);
+                    state.agendas.splice(ind, ind + 1);
+                }
+            }
+            
         },
         toggleCompleteAgenda: (state, action) => {
             const { agendaId, completed } = action.payload;
-            state.agendas[agendaId].completed = !completed;
+            for (let a of state.agendas) {
+                if (a.id === agendaId)
+                    a.completed = !completed;
+            }
+            // state.agendas[agendaId].completed = !completed;
+        },
+        changePage: (state, action) => {
+            const { selectedPage, perPage } = action.payload;
+            const offset = (selectedPage-1) * perPage;
+            state.displayedAgendas = Object.values(state.agendas).slice(offset, offset + perPage);
+            return state;
         }
     }
 });
 
-export const { addAgenda, removeAgenda, toggleCompleteAgenda } = agendasSlice.actions;
+export const { addAgenda, removeAgenda, toggleCompleteAgenda, changePage } = agendasSlice.actions;
 export default agendasSlice.reducer;
 
 export const selectAgendas = state => state.agendas.agendas;
+export const selectDisplayedAgendas = state => state.agendas.displayedAgendas;
